20513e943ec4b125b16f18ad53cf00b7a265537a
Alek Fleischer <thecrazyal02@gmail.com>
2017-01-24 16:53:42 -0800

    Header color now changes based on date

--- js/headertint.js
+++ js/headertint.js
@@ -4,6 +4,5 @@ document.addEventListener('DOMContentLoaded', function() {
 	var colors = ["0, 34, 68", "0, 68, 68", "0, 68, 34", "0, 68, 0", "34, 68, 0", "85, 68, 0", "68, 34, 0", "68, 0, 0", "68, 0, 34", "68, 0, 68", "34, 0, 68", "0, 0, 68"]; // Awful nonsense
 	var d = new Date();
 
-	document.getElementsByClassName("site-header").style.boxShadow = "inset 0 0 0 1000px rgba(0, 34, 38, .5)";
-	colors[d.getMonth()]
+	document.getElementById("tintBox").style.boxShadow = "inset 0 0 0 1000px rgba(" + colors[d.getMonth()] + ", .5)";
 });
