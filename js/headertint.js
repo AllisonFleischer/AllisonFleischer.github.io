afbe434111e9dc76ac47d8dcb5f53f6b21b5d20f
Alek Fleischer <thecrazyal02@gmail.com>
2017-02-01 08:47:00 -0800

    Remove yellow from color rotation
    Yellow is the worst color

--- js/headertint.js
+++ js/headertint.js
@@ -1,7 +1,7 @@
 // Changes header tint based on the month. (it's cute)
 
 document.addEventListener('DOMContentLoaded', function() {
-	var colors = ["0, 34, 68", "0, 68, 68", "0, 68, 34", "0, 68, 0", "34, 68, 0", "85, 68, 0", "68, 34, 0", "68, 0, 0", "68, 0, 34", "68, 0, 68", "34, 0, 68", "0, 0, 68"]; // Awful nonsense
+	var colors = ["0, 34, 68", "0, 68, 68", "0, 68, 34", "0, 68, 0", "16, 68, 0", "34, 68, 0", "68, 34, 0", "68, 0, 0", "68, 0, 34", "68, 0, 68", "34, 0, 68", "0, 0, 68"]; // Awful nonsense
 	var d = new Date();
 
 	document.getElementById("tintBox").style.boxShadow = "inset 0 0 0 1000px rgba(" + colors[d.getMonth()] + ", .5)";
